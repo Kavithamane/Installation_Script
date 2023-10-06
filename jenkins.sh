a440b2f66e27e8980d34801829e31e52714aea09
kavitha <kavitha@gamil.com>
2023-10-06 08:33:43 +0000

    jenkins.sh

---
+++ jenkins.sh
@@ -0,0 +1,25 @@
+
+   1. yum update -y  
+
+   2. amazon-linux-extras
+
+   3. amazon-linux-extras install java-openjdk11 -y
+
+   4. java -version
+
+   5. sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+
+   6. sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+
+   7. yum install jenkins -y
+
+   8. which jenkins
+
+   9. service jenkins status
+
+  10. service jenkins start
+
+  11. chkconfig jenkins on
+
+  12. public-IP:8080
+
